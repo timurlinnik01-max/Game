cc51e9b32ec53e6ad15a848bacdf5ee172648a7a
timurlinnik01-max <timurlinnik01@gmail.com>
2026-05-30 16:53:53 +0200

    cos

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pygame
 from player import Player
-from map_elements import Level1
-from map_elements import Level2
-from map_elements import Level3
+from levels import Level1
+from levels import Level2
+from levels import Level3
 
 import math
 
